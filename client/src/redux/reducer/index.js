838de14ed25e5752533aabf98225178a1e2539d4
Soy Humint <kevinrodriguez.contacto@gmail.com>
2022-08-31 20:15:30 -0300

    Boiler plate frontend

---
+++ client/src/redux/reducer/index.js
@@ -0,0 +1,15 @@
+
+const initialState = {
+   recipes: []
+}
+
+
+function rootReducer(state = initialState, action) {
+   switch (action.type) {
+
+      default:
+         break;
+   }
+}
+
+export default rootReducer;
